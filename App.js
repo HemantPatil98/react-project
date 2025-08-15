c7e3a32238e0666b84bcaf288ad0ef353eab392a
HemantPatil98 <75671081+HemantPatil98@users.noreply.github.com>
2025-08-15 20:20:16 +0530

    react component syntax

--- App.js
+++ App.js
@@ -3,6 +3,16 @@ import { createRoot } from "react-dom/client";
 
 //JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLElement (render)
 const jsxHeading = <h1 className="heading">Hi There...👋🏻</h1>;
-console.log(jsxHeading);
+
+//React Component
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      <h1 className="heading">Hi This is a heading component..</h1>
+    </div>
+  );
+};
 const root = createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+
+//React components needs to be rendered with angular brackets
+root.render(<HeadingComponent />);
