92c97e73b15ac8e594879b64a53109212134e842
HemantPatil98 <75671081+HemantPatil98@users.noreply.github.com>
2025-08-15 19:33:13 +0530

    Heading using JSX

--- App.js
+++ App.js
@@ -1,34 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: 1 }, [
-    React.createElement(
-      "h1",
-      { id: "headingA1", key: 1.1 },
-      "Nested heading from 1"
-    ),
-    React.createElement(
-      "h2",
-      { id: "headingA2", key: 1.2 },
-      "Nested sub heading from 1"
-    ),
-  ]),
-  React.createElement("div", { id: "child2", key: 2 }, [
-    React.createElement(
-      "h1",
-      { id: "headingB1", key: 2.1 },
-      "Nested heading from 2"
-    ),
-    React.createElement(
-      "h2",
-      { id: "headingB2", key: 2.2 },
-      "Nested sub heading from 2"
-    ),
-  ]),
-]);
-const heading = React.createElement("h1", {}, "Hi There");
+//JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLElement (render)
+const jsxHeading = <h1 className="heading">Hi There...👋🏻</h1>;
+console.log(jsxHeading);
 const root = createRoot(document.getElementById("root"));
-console.log("heading Element", heading);
-console.log("root Element", root);
-root.render(parent);
+root.render(jsxHeading);
