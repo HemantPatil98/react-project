fab9fa0c9d61a669a22abf45b873764cfded6250
HemantPatil98 <75671081+HemantPatil98@users.noreply.github.com>
2025-08-15 20:32:56 +0530

    react component alternate syntax and omponent composition

--- App.js
+++ App.js
@@ -4,14 +4,21 @@ import { createRoot } from "react-dom/client";
 //JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLElement (render)
 const jsxHeading = <h1 className="heading">Hi There...👋🏻</h1>;
 
+//React component syntax => withoout {} and return
+const Title = () => <h1 className="title">Hi There...👋🏻</h1>;
+
 //React Component
 const HeadingComponent = () => {
   return (
     <div className="container">
-      <h1 className="heading">Hi This is a heading component..</h1>
+      {/* component composition => nesting one react component into another. */}
+      <Title />
+      <h2 className="heading">This is a heading component..</h2>
     </div>
   );
 };
+
+console.log(HeadingComponent);
 const root = createRoot(document.getElementById("root"));
 
 //React components needs to be rendered with angular brackets
